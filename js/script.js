510a59d40456c99b58fafb0467a764ef81b7901f
Jagoda Pogroszewsla <pogroszewskadesign@gmail.com>
2023-08-19 17:36:03 +0200

    clicked element

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this
   console.log('Link was clicked!');
-  console.log('event');
+  
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
@@ -12,8 +13,10 @@ for(let activeLink of activeLinks){
 }
 
   /* add class 'active' to the clicked link */
+    clickedElement.classList.add('activ');
+    console.log('clickedElement',clickedElement);
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
