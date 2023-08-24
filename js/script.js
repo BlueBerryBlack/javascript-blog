516b5e623c4c5b30ca3f7aec4e6dac78b0aa8336
Jagoda Pogroszewsla <pogroszewskadesign@gmail.com>
2023-08-24 16:11:06 +0200

    for each article

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this
-  console.log('Link was clicked!');
+  console.log('Link was clicked!', event);
   
 
-  /* [DONE] remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active');
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
-
-  /* add class 'active' to the clicked link */
-    clickedElement.classList.add('activ');
-    console.log('clickedElement',clickedElement);
-
-  /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts .active');
-
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+      for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+    /* add class 'active' to the clicked link */
+      clickedElement.classList.add('activ');
+      console.log('clickedElement',clickedElement);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] remove class 'active' from all articles */
+      const activeArticles = document.querySelectorAll('.posts .active');
 
-  /* add class 'active' to the correct article */
+        for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+
+    /* get 'href' attribute from the clicked link */
+      const articleSelector = clickedElement.getAttribute('href');
+      console.log('articleSelector', articleSelector);
+      
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+      const targetArticle = document.querySelector(articleSelector);
+      console.log('targetArticle', targetArticle);
+
+
+    /* add class 'active' to the correct article */
+      targetArticle.classList.add('active');
+      console.log('targetArticle',targetArticle);
 }
 
 const links = document.querySelectorAll('.titles a');
@@ -35,3 +44,22 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+
+
+const optArticleSelector = '.posts',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+
+  /*remove contents of titleList*/
+  const titleList = document.querySelector(optTitleListSelector);
+  console.log('titleList', titleList);
+  titleList.innerHTML ='';
+
+  
+
+} 
+
+generateTitleLinks();
